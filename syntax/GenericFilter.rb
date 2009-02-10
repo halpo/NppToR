61d4e120eba4a260b0979d94abb4aa18c445ae17
halpo <halpo@9f7bfecc-9004-11dd-b05a-a9686588ba97>
2009-02-10 14:16:58 +0000

    GenericFilter.rb should be finished.  GenericFilter is a class that filters keyword lists for S3 generic and S3 specific methods.
    git-svn-id: https://npptor.svn.sourceforge.net/svnroot/npptor@45 9f7bfecc-9004-11dd-b05a-a9686588ba97

--- syntax/GenericFilter.rb
+++ syntax/GenericFilter.rb
@@ -2,24 +2,80 @@ require 'rinruby'
 R.quit
 
 class GenericFilter
-	
-	def initialize()
-		@myR = RinRuby.new(echo=false,interactive=false)
-@myR.eval <<SETGENERIC
-		is.generic<-function(names){
-			sapply(names,function(n)
-				tryCatch(if(length(methods(n))>0) TRUE else NA,
-				error=function(e)NA,
-				warning=function(w)NA))
-		}
-SETGENERIC
+	def initialize(keywords=nil, extragenerics=Array.new ,r_exe=nil)
+		@extragenerics = extragenerics
+		@generics=Array.new
+		@specifics=Array.new
+		@myR = RinRuby.new(echo=false,interactive=false,executable=r_exe)
+		@myR.eval <<-ISGENERIC
+				is.generic<-function(names){
+					sapply(names,function(n)
+						tryCatch(ifelse(length(methods(n))>0,TRUE,NA),
+						error=function(e)NA,
+						warning=function(w)NA))
+				}
+		ISGENERIC
+		# BEGIN FILTERING LIST OF KEYWORDS TO PARTS
+		if(!keywords.nil?)
+			keywords.delete_if{ |key|
+				if(isgeneric?(key))
+					@generics << key
+					true
+				else false end
+			}
+			keywords.delete_if{ |key|
+				if(isspecific?(key))
+					@specifics << key
+					true
+				else false end
+			}
+			@others = keywords
+		else
+			@generics=nil
+			@others=nil
+			@specifics=nil
+		end
+	end
+public
+	def allgenerics()
+		@extragenerics+@generics
 	end
 	def isgeneric?(names)
 		@myR.fnames = names
 		@myR.eval "(isS3<-as.integer(is.generic(fnames)))"
 		index = @myR.pull 'isS3'
 	end
-	def generics(names)
-		nisgeneric?(names)
-		
+	def isspecific?(name)
+		allgenerics().any?{|generic|
+			name =~ /^#{generic.gsub(/\./,'\\\.')}\.[\.\w]+/
+		}
+	end
+public
+	def S3generics()
+		@generics
+	end
+	def S3specifics()
+		@specifics
+	end
+	def filtered()
+		@generics+@others
+	end
 end
+##  TESTING
+# words= %w[alias.formula
+# alias.lm
+# alist
+# all
+# all.equal
+# all.equal.POSIXct
+# all.equal.character
+# all.equal.default
+# all.equal.factor]
+
+# gf=GenericFilter.new(keywords=words)
+# puts "Generics:"
+# puts gf.S3generics
+# puts "filtered:"
+# puts gf.filtered
+# puts gf.isgeneric?('all.equal')
+# puts gf.isspecific?('all.equal.factor')
