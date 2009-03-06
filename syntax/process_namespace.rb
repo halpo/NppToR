1b048006f69169777a7fa47e579e79ab959c3d9f
halpo <halpo@9f7bfecc-9004-11dd-b05a-a9686588ba97>
2009-03-06 05:22:24 +0000

    git-svn-id: https://npptor.svn.sourceforge.net/svnroot/npptor@61 9f7bfecc-9004-11dd-b05a-a9686588ba97

--- syntax/process_namespace.rb
+++ syntax/process_namespace.rb
@@ -1,14 +1,16 @@
 
-basenamespace<-grep("^[_a-zA-z0-9\\.]*$",ls(.BaseNamespaceEnv),value=TRUE)
+basenamespace<-grep("^[a-zA-z]+[_a-zA-z0-9\\.]*$",ls(.BaseNamespaceEnv),value=TRUE)
 
 methodslist<-character(0)
 for(generic in names(.knownS3Generics))methodslist<-append(methodslist,methods(generic))
-
 setdiff(basenamespace,methodslist)
 
+pkgname="stats"
+pkgmethods<-get('S3methods',envir=get('.__NAMESPACE__.',envir=asNamespace(pkgname)))
+pkgexports<-grep("^[a-zA-z]+[_a-zA-z0-9\\.]*$",ls(get('exports',envir=get('.__NAMESPACE__.',envir=asNamespace(pkgname))),all.names=T),value=TRUE)
+setdiff(pkgexports,pkgmethods)
 
-# get('S3methods',envir=get('.__NAMESPACE__.',envir=asNamespace("tcltk")))
-# ls(get('exports',envir=get('.__NAMESPACE__.',envir=asNamespace("tcltk"))),all.names=T)
+installed.packages()[,c('Package','Priority')]
 
 
 # def process_NAMESPACE(filename)
