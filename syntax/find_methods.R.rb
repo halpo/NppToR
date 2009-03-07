629d4b976c956151c2dffdbb3daf0aba72f0c41d
halpo <halpo@9f7bfecc-9004-11dd-b05a-a9686588ba97>
2009-03-07 20:37:10 +0000

    added find_methods.R.rb to hold the defintions for get_base_methods get_namespace and get_pkg_methods necessary for process_namespace.rb, requires definitions in methods.R.rb to work properly.
    git-svn-id: https://npptor.svn.sourceforge.net/svnroot/npptor@68 9f7bfecc-9004-11dd-b05a-a9686588ba97

---
+++ syntax/find_methods.R.rb
@@ -0,0 +1,28 @@
+find_methods=<<RCODEFINDMETHODS
+	get_base_methods<-function(S3Methods=FALSE){
+		basenamespace<-grep("^[a-zA-z]+[_a-zA-z0-9\\\\.]*$",ls(.BaseNamespaceEnv),value=TRUE)
+		if(S3Methods)return(basenamespace);end<-FALSE  # end here is purely for syntax highlighting in notepad++
+		methodslist<-character(0)
+		for(generic in names(.knownS3Generics))methodslist<-append(methodslist,methods(generic));end<-FALSE
+		setdiff(basenamespace,methodslist);
+	}
+	get_namespace<-function(pkgname)get('.__NAMESPACE__.',envir=asNamespace(pkgname),mode="environment")
+	get_pkg_methods<-function(pkgname, S3Methods=FALSE){
+		if(pkgname=='base')return(get_base_methods(S3Methods));end<-FALSE	
+		pkgnamespace<-try(get_namespace(pkgname),TRUE)
+		if(class(pkgnamespace)=="try-error"  && installed.packages()[pkgname,'Priority']=="base") {
+			if(pkgname=='datasets')stop("pkg 'datasets' has no namespace.")
+			pkgexports<-grep("^[a-zA-z]+[_a-zA-z0-9\\\\.]*$",ls(asNamespace(pkgname)),value=TRUE)
+			for(i in 1:length(pkgexports)){
+				if(i>length(pkgexports)) break
+				generic<-is.S3Generic(pkgexports[i])
+				pkgexports<-setdiff(pkgexports,attr(generic,'methods'))				
+			}
+			pkgexports
+		} else stop(gettextf("The package '%s' doen not have defined namespace nor is it a base package.",pkgname))
+		pkgexports<-grep("^[a-zA-z]+[_a-zA-z0-9\\\\.]*$",ls(get('exports',envir=pkgnamespace)),value=TRUE)
+		if(S3Methods)return(pkgexports);end<-FALSE
+		pkgmethods<-get('S3methods',envir=get_namespace(pkgname))
+		setdiff(pkgexports,pkgmethods)
+	}
+RCODEFINDMETHODS
